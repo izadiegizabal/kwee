12f392e98669cfd52f45a353d0ce96b2196b3d8c
Izadi Egizabal Alkorta <64izadi@gmail.com>
2018-10-29 16:40:21 +0100

    Initial Angular project set-up

---
+++ SPA/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+// Angular Material modules
+import {MatCardModule} from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    // Angular Material modules
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
