795f559b4fd915666af6fafe58daa9314f651880
Izadi Egizabal Alkorta <64izadi@gmail.com>
2018-11-02 14:13:02 +0100

    Basic offers card (responsive)

--- SPA/src/app/offer/offer-preview-card/offer-preview-card.component.ts
+++ SPA/src/app/offer/offer-preview-card/offer-preview-card.component.ts
@@ -8,10 +8,21 @@ import {IconWithTextComponent} from '../../common/icon-with-text/icon-with-text.
   styleUrls: ['./offer-preview-card.component.scss']
 })
 export class OfferPreviewCardComponent implements OnInit {
+  position = 'Senior SEO Consultant';
+  description = 'A search engine optimization consultant (or SEO consultant) is a job that analyzes and reviews websites and\n' +
+    '      their incoming links in order to provide expert advice, guidance, and recommendations to business owners\n' +
+    '      seeking to earn more natural search engine traffic and higher ranking positions.A search engine optimization consultant ' +
+    '(or SEO consultant) is a job that analyzes and reviews websites and\n' +
+    '      their incoming links in order to provide expert advice, guidance, and recommendations to business owners\n' +
+    '      seeking to earn more natural search engine traffic and higher ranking positions.';
+
   constructor() {
   }
 
   ngOnInit() {
+    if (this.description.length > 280) {
+      this.description = this.description.substr(0, 280) + '...';
+    }
   }
 
 }
--- SPA/src/app/offerer/offerer-name-overview/offerer-name-overview.component.ts
+++ SPA/src/app/offerer/offerer-name-overview/offerer-name-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-offerer-name-overview',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offerer-name-overview.component.scss']
 })
 export class OffererNameOverviewComponent implements OnInit {
+  @Input() index: number;
+  @Input() name: string;
 
   constructor() { }
 
