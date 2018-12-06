85e31f02bd02c8ec9905ccd12aebdf798de542f3
Carlos <carlosaldaravi@gmail.com>
2018-12-06 01:13:02 +0100

    update errorHandlet.js
    Added 'ok: false' to json before error message

--- apirest/backend/src/middlewares/errorHandlet.js
+++ apirest/backend/src/middlewares/errorHandlet.js
@@ -2,5 +2,5 @@ const colors = require('../tools/colors');
 
 module.exports = function errorHandler(error, req, res, next) {
     console.log(colors.redBright, 'ERROR: ' + colors.reset, error.error);
-    res.status(error.status || 400).json({ error: error.error });
+    res.status(error.status || 400).json({ ok: 'false', error: error.error });
 };
