0d6eafd0d5e963467f0293a69969d84bc4153455
Carlos <carlosaldaravi@gmail.com>
2019-01-05 19:07:31 +0100

    new package express-fileupload

--- apirest/src/router/index.js
+++ apirest/src/router/index.js
@@ -26,6 +26,7 @@ const routes = [
     require('./routes/login/linkedin'),
     require('./routes/login/twitter'),
     require('./routes/logs'),
+    require('./routes/uploads'),
     // require('./routes/login/instagram'),
     // require('./routes/login/telegram'),
 ];
