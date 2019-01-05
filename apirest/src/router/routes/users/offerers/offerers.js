33d6a5a92320a86340ec6be143b3334a139ba59b
Carlos <carlosaldaravi@gmail.com>
2019-01-05 13:45:20 +0100

    saving log of offerers

--- apirest/src/router/routes/users/offerers/offerers.js
+++ apirest/src/router/routes/users/offerers/offerers.js
@@ -10,7 +10,7 @@ module.exports = (app, db) => {
 
     // GET all users offerers
     app.get('/offerers', async(req, res, next) => {
-
+        await logger.saveLog('GET', 'offerers', null, res);
         try {
             let users = await db.users.findAll();
             let offerers = await db.offerers.findAll();
@@ -52,7 +52,7 @@ module.exports = (app, db) => {
     // GET one offerer by id
     app.get('/offerer/:id([0-9]+)', async(req, res, next) => {
         const id = req.params.id;
-        console.log("id" + id);
+        await logger.saveLog('GET', 'offerer', id, res);
         try {
             let users = await db.users.findOne({
                 where: { id }
@@ -99,6 +99,7 @@ module.exports = (app, db) => {
 
     // POST single offerer
     app.post('/offerer', async(req, res, next) => {
+        await logger.saveLog('POST', 'offerer', null, res);
         let transaction;
 
         try {
@@ -137,10 +138,12 @@ module.exports = (app, db) => {
 
     // Update offerer by themself
     app.put('/offerer', async(req, res, next) => {
+        let logId = await logger.saveLog('PUT', 'offerer', null, res);
         const updates = req.body;
 
         try {
             let id = tokenId.getTokenId(req.get('token'));
+            logger.updateLog(logId, id);
             updateOfferer(id, updates, res);
         } catch (err) {
             next({ type: 'error', error: err.message });
@@ -151,6 +154,7 @@ module.exports = (app, db) => {
     app.put('/offerer/:id([0-9]+)', [checkToken, checkAdmin], async(req, res, next) => {
         const id = req.params.id;
         const updates = req.body;
+        await logger.saveLog('PUT', 'offerer', id, res);
 
         try {
             updateOfferer(id, updates, res);
@@ -162,6 +166,7 @@ module.exports = (app, db) => {
     // DELETE
     app.delete('/offerer/:id([0-9]+)', [checkToken, checkAdmin], async(req, res, next) => {
         const id = req.params.id;
+        await logger.saveLog('DELETE', 'offerer', id, res);
 
         try {
             let offerer = await db.offerers.findOne({
