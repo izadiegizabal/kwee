4ba11eb7c609b3b3175e59eab5962bd84b8b7e03
Albagaller <alba.g.aller@gmail.com>
2019-01-30 20:39:29 +0100

    Added profiles store

--- SPA/src/app/shared/profiles/candidate-profile/candidate-profile.component.ts
+++ SPA/src/app/shared/profiles/candidate-profile/candidate-profile.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {select, Store} from '@ngrx/store';
+import * as fromApp from '../../../store/app.reducers';
+import * as ProfilesActions from './../store/profiles.actions';
+import * as fromProfiles from './../store/profiles.reducers';
+import {UtilsService} from '../../utils.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-candidate-profile',
@@ -7,9 +14,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandidateProfileComponent implements OnInit {
 
-  constructor() { }
+  profilesState: Observable<fromProfiles.State>;
+
+  constructor(private _utils: UtilsService, private store$: Store<fromApp.AppState>, private activatedRoute: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    const params = this.activatedRoute.snapshot.params;
+    // console.log(params.id);
+    this.store$.dispatch(new ProfilesActions.TryGetProfileCandidate({id: params.id}));
+    this.profilesState = this.store$.pipe(select(state => state.profiles));
+    // this.store$.dispatch(new ProfilesActions.TryGetProfileOfferer({id: params.id}));
+    // this.profilesState = this.store$.pipe(select(state => state.profiles));
   }
 
 }
--- SPA/src/app/shared/profiles/candidate-profile/candidate-profile.module.ts
+++ SPA/src/app/shared/profiles/candidate-profile/candidate-profile.module.ts
@@ -2,14 +2,22 @@ import {NgModule} from '@angular/core';
 import {CandidateProfileComponent} from './candidate-profile.component';
 import {SharedModule} from '../../shared.module';
 import {CandidateProfileRoutingModule} from './candidate-profile-routing.module';
+import {profilesReducer} from './../store/profiles.reducers';
+import {ProfilesEffects} from './../store/profiles.effects';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
 
 @NgModule({
   declarations: [CandidateProfileComponent],
   imports: [
     SharedModule,
-    CandidateProfileRoutingModule
+    CandidateProfileRoutingModule,
+    StoreModule.forFeature('profiles', profilesReducer),
+    EffectsModule.forFeature([ProfilesEffects])
   ]
 })
 export class CandidateProfileModule {
 
 }
+
+
