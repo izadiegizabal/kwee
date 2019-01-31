224b377f93a310f02b3cc26a5c8516d3fc18e333
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-01-31 16:30:45 +0100

    Started with the opinions of candidate profile

--- SPA/src/app/shared/profiles/candidate-profile/candidate-profile.module.ts
+++ SPA/src/app/shared/profiles/candidate-profile/candidate-profile.module.ts
@@ -3,16 +3,21 @@ import {CandidateProfileComponent} from './candidate-profile.component';
 import {SharedModule} from '../../shared.module';
 import {CandidateProfileRoutingModule} from './candidate-profile-routing.module';
 import {OffererNameOverviewModule} from '../../offerer-name-overview/offerer-name-overview.module';
-import { CandidateMoreInfoComponent } from './candidate-more-info/candidate-more-info.component';
+import {CandidateMoreInfoComponent} from './candidate-more-info/candidate-more-info.component';
 import {ExperienceFormsModule} from '../../../auth/signup/signup-candidate/experience-forms/experience-forms.module';
 import {EducationsFormsModule} from '../../../auth/signup/signup-candidate/education-forms/educations-forms.module';
-import {profilesReducer} from './../store/profiles.reducers';
-import {ProfilesEffects} from './../store/profiles.effects';
+import {profilesReducer} from '../store/profiles.reducers';
+import {ProfilesEffects} from '../store/profiles.effects';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
+import {CandidateProfileOpinionsComponent} from './candidate-profile-opinions/candidate-profile-opinions.component';
 
 @NgModule({
-  declarations: [CandidateProfileComponent, CandidateMoreInfoComponent],
+  declarations: [
+    CandidateProfileComponent,
+    CandidateMoreInfoComponent,
+    CandidateProfileOpinionsComponent,
+  ],
   imports: [
     SharedModule,
     CandidateProfileRoutingModule,
