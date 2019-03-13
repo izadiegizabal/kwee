85e439ff2df0f7f4ed87cb9639e011b0b70e7281
Carlos <carlosaldaravi@gmail.com>
2019-03-13 15:13:25 +0100

    update default status application

--- apirest/src/models/applications.js
+++ apirest/src/models/applications.js
@@ -40,7 +40,7 @@ module.exports = (sequelize, DataTypes) => {
         status: {
             type: DataTypes.INTEGER,
             field: 'status',
-            defaultValue: 1,
+            defaultValue: 0,
             allowNull: false,
             validate: {
                 isIn: {
