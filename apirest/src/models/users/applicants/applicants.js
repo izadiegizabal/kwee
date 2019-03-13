a3882f12de0ccfa6f6675eb75adb9dc1e44c7475
Marcos Urios <marcosuriosgomez@gmail.com>
2019-03-13 18:03:45 +0100

    Model applicant and offerer fixed

--- apirest/src/models/users/applicants/applicants.js
+++ apirest/src/models/users/applicants/applicants.js
@@ -97,6 +97,15 @@ module.exports = (sequelize, DataTypes) => {
         }
       },
 
+      satisfactionAVG: {
+        type: DataTypes.FLOAT(3, 2),
+        allowNull: false,
+        defaultValue: 0,
+        validate: {
+          isFloat: true
+        }
+      },
+
       nOpinions: {
         type: DataTypes.INTEGER,
         allowNull: false,
