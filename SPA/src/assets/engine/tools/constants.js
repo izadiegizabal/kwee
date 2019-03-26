02551853e7dd9185aa32623567da8c550db7733b
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-03-26 19:27:08 +0100

    Fixed environments

--- SPA/src/assets/engine/tools/constants.js
+++ SPA/src/assets/engine/tools/constants.js
@@ -3,7 +3,7 @@ const constants = {};
 
 constants.ENVIRONMENT = window.location.hostname;
 
-constants.URL = constants.ENVIRONMENT==='localhost'?'http://localhost:4200':constants.ENVIRONMENT==='h203.eps.ua.es'?'http://www.h203.eps.ua.es':'https://www.kwee.ovh';
+constants.URL = constants.ENVIRONMENT==='localhost'?'http://localhost:4200':constants.ENVIRONMENT==='h203.eps.ua.es'?'http://h203.eps.ua.es':'https://www.kwee.ovh';
 
 export {
     constants
