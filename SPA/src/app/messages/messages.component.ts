c96bc2bee81097db66935ea25bab5d2a589bc99b
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-05-22 17:08:53 +0200

    Start of messages revamped + optimisations

--- SPA/src/app/messages/messages.component.ts
+++ SPA/src/app/messages/messages.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MessagesService} from './messages.service';
-import {WebsocketService} from '../sockets.io/websocket.service';
 import {Observable} from 'rxjs';
 import * as MessageActions from './store/message.actions';
 import {select, Store} from '@ngrx/store';
@@ -8,6 +7,9 @@ import * as fromApp from '../store/app.reducers';
 import * as fromMessages from './store/message.reducers';
 import * as moment from 'moment';
 import {Title} from '@angular/platform-browser';
+import {MatSidenav} from '@angular/material';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {ActivatedRoute} from '@angular/router';
 
 export interface Message {
   senderId: number;
@@ -36,14 +38,25 @@ export class MessagesComponent implements OnInit {
   public messagesState: Observable<fromMessages.State>;
   public messagesState2: Observable<fromMessages.State>;
 
+  @ViewChild('drawer') drawer: MatSidenav;
+
   sub;
 
   bdMessages: any[] = [];
   areMessages = false;
-  userSelected = false;
+  isUserSelected = false;
   userList: Users[] = [];
   differentUsers: number;
-  messageToSend: Message;
+  messageToSend = {
+    date: '',
+    hour: '',
+    message: '',
+    receiverId: 0,
+    receiverName: '',
+    senderId: 0,
+    senderName: '',
+    total: 0
+  };
 
   authState: any;
   authUser: any;
@@ -61,12 +74,14 @@ export class MessagesComponent implements OnInit {
   constructor(
     private titleService: Title,
     public messageService: MessagesService,
-    public wsService: WebsocketService,
+    public media: BreakpointObserver,
+    private activatedRoute: ActivatedRoute,
     private store$: Store<fromApp.AppState>
   ) {
   }
 
   ngOnInit() {
+
     this.titleService.setTitle('Kwee - Messages');
 
     this.authState = this.store$.pipe(select('auth'));
@@ -81,8 +96,6 @@ export class MessagesComponent implements OnInit {
       console.log('Mensaje recibido');
       console.log(msg);
       this.bdMessages.push(msg);
-
-
     });
 
     this.store$.dispatch(new MessageActions.TryGetMessages({}));
@@ -90,25 +103,30 @@ export class MessagesComponent implements OnInit {
     this.sub = this.store$.pipe(select(state => state.messages)).subscribe(
       (message) => {
         if (message.messages && message.messages.total > 0) {
-
           this.differentUsers = message.messages.total;
           this.userList = message.messages.data;
           this.areMessages = true;
-
         }
-
       });
 
+    this.activatedRoute.params.subscribe((params) => {
+      if (!isNaN(Number(params['id']))) {
+        this.selectUser(Number(params['id']));
 
+        // TODO: fetch who this is to update the user list and fix so that everything works
+      }
+    });
   }
 
-  // lazyLoad angular 7
-  // cdk-virtual-scroll
+  selectUser(id: number) {
 
-  // tslint:disable-next-line:use-life-cycle-interface
-  selectUser(id) {
+    console.log('trying to get conversation from id: ' + id);
+    // TODO: Update param id
+
+    this.closeDrawerIfMobile();
+
     this.sub.unsubscribe();
-    this.userSelected = true;
+    this.isUserSelected = true;
     this.store$.dispatch(new MessageActions.TryGetConversation({id}));
 
     this.store$.pipe(select(state => state.messages)).subscribe(
@@ -117,7 +135,10 @@ export class MessagesComponent implements OnInit {
         if (conver.messages && conver.messages.total > 0) {
 
           this.bdMessages = conver.messages.data;
-          this.messageToSend = this.bdMessages[0];
+
+          if (this.bdMessages[0]) {
+            this.messageToSend = this.bdMessages[0];
+          }
 
           if (this.messageToSend.senderId !== this.authUser.id) {
             this.messageToSend.receiverId = this.messageToSend.senderId;
@@ -157,5 +178,15 @@ export class MessagesComponent implements OnInit {
 
   initMessage() {
     this.messageToSend = JSON.parse(JSON.stringify(this.messageToSend));
+  }
+
+  isMobile() {
+    return !this.media.isMatched('screen and (min-width: 599px)'); // gt-sm
+  }
+
+  closeDrawerIfMobile() {
+    if (this.isMobile()) {
+      this.drawer.toggle();
+    }
   }
 }
