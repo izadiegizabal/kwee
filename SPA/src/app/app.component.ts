3e4559206e4c10d9eb1172ee65a8ebcb88bd7bed
Carlos <carlosaldaravi@gmail.com>
2019-05-22 20:16:09 +0200

    notifications working

--- SPA/src/app/app.component.ts
+++ SPA/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {LandingComponent} from './landing/landing.component';
 import {NavigationEnd, Router} from '@angular/router';
+import { NotificationsService } from './notifications/notifications.service';
+import { MessagesService } from './messages/messages.service';
 
 // Google Analytics
 declare let ga: Function;
@@ -15,7 +17,10 @@ export class AppComponent implements OnInit {
 
   isLanding = false;
 
-  constructor(public router: Router) {
+  constructor(public router: Router,
+              public notifications: NotificationsService,
+              public messages: MessagesService) {
+
     // Subscribe to router events and send page views to Google Analytics
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
