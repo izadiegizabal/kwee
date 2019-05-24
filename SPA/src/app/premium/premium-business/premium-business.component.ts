339f7d3c5a466fa373473483684f44f51baca3bc
Alba González Aller <alba.g.aller@gmail.com>
2019-05-24 14:11:31 +0200

    Alert premium

--- SPA/src/app/premium/premium-business/premium-business.component.ts
+++ SPA/src/app/premium/premium-business/premium-business.component.ts
@@ -5,6 +5,7 @@ import {select, Store} from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import {Observable} from 'rxjs';
 import * as fromAuth from '../../auth/store/auth.reducers';
+import {AlertDialogComponent} from '../../shared/alert-dialog/alert-dialog.component';
 
 @Component({
   selector: 'app-premium-business',
@@ -42,5 +43,15 @@ export class PremiumBusinessComponent implements OnInit {
       }
     });
   }
+
+  alert() {
+    this.dialog.open(AlertDialogComponent, {
+      data: {
+        header: 'If you are sure to delete your premium subscription, ' +
+          'your account will lose premium privileges at the end of the month.',
+      }
+    });
+  }
+
 
 }
