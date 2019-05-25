bc6f0c57472fc161ed762d7fc3ea8b17295b1df0
Carlos <carlosaldaravi@gmail.com>
2019-05-25 22:01:43 +0200

    catch validation errors when post offer

--- SPA/src/app/admin/user-log/user-log.component.ts
+++ SPA/src/app/admin/user-log/user-log.component.ts
@@ -7,14 +7,23 @@ export interface DialogData {
   id: number;
 }
 
-@Component({
-  selector: 'app-user-log',
-  templateUrl: './user-log.component.html',
-  styleUrls: ['./user-log.component.scss']
-})
-export class UserLogComponent implements OnInit {
+export interface LogElement {
+  order?: number;
+  action: string;
+  ip: string;
+  os: string;
+  browser: string;
+}
 
-  logs: any = [];
+@Component({
+    selector: 'app-user-log',
+    templateUrl: './user-log.component.html',
+    styleUrls: ['./user-log.component.scss']
+  })
+export class UserLogComponent implements OnInit {
+  displayedColumns: string[] = ['order', 'action', 'ip', 'platform'];
+  element_data: LogElement[] = [];
+  dataSource: any = [];
 
   constructor(@Inject(MAT_DIALOG_DATA)
               public data: DialogData,
@@ -26,14 +35,24 @@ export class UserLogComponent implements OnInit {
     this._ngZone.runOutsideAngular( () => {
     const headers = new HttpHeaders().set('Content-Type', 'application/json');
     console.log('id: ', this.data.id);
+    console.log('a: ' + environment.apiUrl);
+
       this.httpClient.get(environment.apiUrl + 'logs/' + this.data.id, {headers: headers}).subscribe(
-        res => {
+        (res: any) => {
           this._ngZone.run(() => {
-            this.logs = res;
-            console.log('logs: ', this.logs);
+            res.logs.forEach(element => {
+              this.dataSource.push({
+                order: '0',
+                action: element.action,
+                ip: element.ip,
+                os: element.useragent.os,
+                browser: element.useragent.browser
+              });
+            });
+            console.log('dataSource: ', this.dataSource);
           });
         },
-        err => console.log('error')
+        err => console.log('error', err)
       );
     });
   }
