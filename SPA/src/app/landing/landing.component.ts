d324d47becb628c9a2b63d1209d7b4a986bbd448
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-05-26 12:33:27 +0200

    Optimised landing code

--- SPA/src/app/landing/landing.component.ts
+++ SPA/src/app/landing/landing.component.ts
@@ -1,19 +1,18 @@
 import {Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
-import {shared, cleanEngine} from '../../assets/engine/commons';
-import {allowActions, mainInit, mainR, resetCanvas, setSceneWidth} from '../../assets/engine/main';
+import {cleanEngine, mango, shared} from '../../assets/engine/commons';
+import {allowActions, mainInit, mainR, resetCanvas} from '../../assets/engine/main';
 import {Router} from '@angular/router';
 import {Title} from '@angular/platform-browser';
-import * as OffersActions from "../offer/store/offers.actions";
-import {select, Store} from "@ngrx/store";
-import * as fromApp from "../store/app.reducers";
-import {Observable} from "rxjs";
-import * as fromOffers from "../offer/store/offers.reducers";
+import * as OffersActions from '../offer/store/offers.actions';
+import {select, Store} from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import {Observable} from 'rxjs';
+import * as fromOffers from '../offer/store/offers.reducers';
 import {environment} from '../../environments/environment';
 
-import { TMotorTAG } from '../../assets/engine/TMotorTAG';
-import { TResourceManager } from '../../assets/engine/resourceManager';
-import { mango } from '../../assets/engine/commons';
+import {TMotorTAG} from '../../assets/engine/TMotorTAG';
+import {TResourceManager} from '../../assets/engine/resourceManager';
 
 import {ContractType} from '../../models/Offer.model';
 
@@ -32,10 +31,8 @@ export class LandingComponent implements OnInit, OnDestroy {
   disabled: boolean;
   particles: boolean;
 
-  // canvas "global" variables
-  auxCanvas = null;
+  // canvas variables
   canvas = null;
-  context2d = null;
 
   // new width screen size
   changed = 0;
@@ -81,7 +78,7 @@ export class LandingComponent implements OnInit, OnDestroy {
     } else {
       this.screenWidth = 4;
     }
-    if(allowActions.card){
+    if (allowActions.card) {
       this.configCard();
     }
   }
@@ -94,7 +91,7 @@ export class LandingComponent implements OnInit, OnDestroy {
     // Title stuff
     this.titleService.setTitle('Kwee - Home');
     // Data for the engine
-    this.query= {...this.query, status: '0'};
+    this.query = {...this.query, status: '0'};
     this.store$.dispatch(new OffersActions.TryGetOffers({page: 1, limit: 25, params: this.query, order: '0'}));
     this.offersState = this.store$.pipe(select(state => state.offers));
     this.offersState.pipe(
@@ -130,7 +127,7 @@ export class LandingComponent implements OnInit, OnDestroy {
     this.currentIndex = -1;
 
     // this.context2d.translate(0.5,0.5);
-    let width  = window.innerWidth;
+    const width = window.innerWidth;
     if (width <= 599) {
       this.screenWidth = 1;
     } else if (width > 599 && width <= 850) {
@@ -165,14 +162,14 @@ export class LandingComponent implements OnInit, OnDestroy {
     const landMaterial = this.motor.createMaterial(
       /* color */    [0.258, 0.960, 0.6, 1.0],
       /* specular */ [1.0, 1.0, 1.0, 1.0],
-      /* shiny */    3 );
+      /* shiny */    3);
     const LOD_earth = this.motor.dynamicMeshArrayLazyLoading(this.scene, ['0_earth.json', '2_earth_SS.json'], landMaterial);
 
     // Sea
     const seaMaterial = this.motor.createMaterial(
       /* color */    [0.313, 0.678, 0.949, 1.0],
       /* specular */ [1.0, 1.0, 1.0, 1.0],
-      /* shiny */    15 );
+      /* shiny */    15);
 
     const LOD_sea = this.motor.dynamicMeshArrayLazyLoading(this.scene, ['0_sea.json', '2_sea_SS.json'], seaMaterial);
 
@@ -182,10 +179,10 @@ export class LandingComponent implements OnInit, OnDestroy {
     const camera = this.motor.createCamera(this.scene);
     this.motor.enableCam(camera);
 
-    let camPos = [];
+    const camPos = [];
 
-    let point = this.motor.get3DfronLatLon(40.415363, -3.707398);
-    mango.targetPoint =  this.motor.get3DfronLatLon(40.415363, -3.707398);
+    const point = this.motor.get3DfronLatLon(40.415363, -3.707398);
+    mango.targetPoint = this.motor.get3DfronLatLon(40.415363, -3.707398);
     allowActions.p = mango.targetPoint;
 
     this.motor.easeCamera();
@@ -193,21 +190,21 @@ export class LandingComponent implements OnInit, OnDestroy {
     camPos.push(point[1] * mango.zoom);
     camPos.push(point[2] * mango.zoom);
 
-    this.motor.cameraLookAt( camera, [...camPos],
-      [0,0,0],
-      [0,1,0]);
+    this.motor.cameraLookAt(camera, [...camPos],
+      [0, 0, 0],
+      [0, 1, 0]);
 
     this.motor.calculateViews();
     mango.auxViewMatrix = mango.viewMatrix;
 
     // ----- LIGHTS -----
-    const light =  this.motor.createLight(this.scene, 1, [0.2, 0.2, 0.2, 1.0],  [1.0, 1.0, 1.0, 1.0],  [0.5, 0.5, 0.5, 1.0], [10.0, 10.0, 10.0]);
+    const light = this.motor.createLight(
+      this.scene,
+      1,
+      [0.2, 0.2, 0.2, 1.0], [1.0, 1.0, 1.0, 1.0], [0.5, 0.5, 0.5, 1.0], [10.0, 10.0, 10.0]);
     this.motor.calculateLights();
 
     // ----- RENDER LOOP -----
-    let number = 0;
-
-    let then = 0;
     let last = 0;
     /*
       Fases
@@ -218,53 +215,60 @@ export class LandingComponent implements OnInit, OnDestroy {
      */
     mango.fase = null;
     const thisContext = this;
-    const loop = function(now) {
-      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    const loop = function (now) {
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
       switch (mango.fase) {
         /// wait 5s
         case 0:
-          if(now - last >= 5000) {
+          if (now - last >= 5000) {
             last = now;
             mango.fase = 1;
-            if(thisContext.currentIndex === (thisContext.fetchedOffers.length - 1)){
+            if (thisContext.currentIndex === (thisContext.fetchedOffers.length - 1)) {
               thisContext.currentIndex = -1;
             }
           }
           break;
         case 1:
-          if(now - last >= 1000) {
+          if (now - last >= 1000) {
             last = now;
             allowActions.card = false;
             if (thisContext.fetchedOffers.length > 0) {
               mango.fase = 2;
               thisContext.currentIndex++;
-              allowActions.random = thisContext.motor.rotateCamToRandomXYOffset(+thisContext.fetchedOffers[thisContext.currentIndex].lat, +thisContext.fetchedOffers[thisContext.currentIndex].lon, 1, thisContext.screenWidth);
+              allowActions.random = thisContext.motor.rotateCamToRandomXYOffset(
+                +thisContext.fetchedOffers[thisContext.currentIndex].lat,
+                +thisContext.fetchedOffers[thisContext.currentIndex].lon,
+                1,
+                thisContext.screenWidth);
             }
           }
           break;
         case 2:
-          if(now - last >= 1500) {
+          if (now - last >= 1500) {
             allowActions.point = thisContext.motor.calculateTarget2Dfrom3DPoint();
             allowActions.card = true;
-            thisContext.motor.createFocus(thisContext.scene, 100, 'straight', mango.targetPoint , 'normal', null, [1,0.25,0.51, 1.0]);
-            let fireworks = thisContext.motor.createFocus(thisContext.scene, 150, 'fireworks', mango.targetPoint , 'normal', null, [1,0.5,0.67, 1.0]);
+            thisContext.motor.createFocus(
+              thisContext.scene, 100, 'straight', mango.targetPoint, 'normal', null, [1, 0.25, 0.51, 1.0]);
+            const fireworks = thisContext.motor.createFocus(
+              thisContext.scene, 150, 'fireworks', mango.targetPoint, 'normal', null, [1, 0.5, 0.67, 1.0]);
             setTimeout(() => {
-              thisContext.motor.deleteFocus(fireworks);
+              if (thisContext.motor) {
+                thisContext.motor.deleteFocus(fireworks);
+              }
             }, 800);
             last = now;
             mango.fase = 0;
           }
           break;
         case 3:
-          if(now - last >= 1000) {
+          if (now - last >= 1000) {
             last = now;
             mango.fase = 1;
           }
           break;
         default:
-          //console.log(-1);
           last = now;
-          if(mango.fase == -1){
+          if (mango.fase === -1) {
             mango.fase = 3;
           }
           break;
@@ -295,8 +299,7 @@ export class LandingComponent implements OnInit, OnDestroy {
 
   configCard() {
 
-    // let point = this.motor.computeCoordenates(this.fetchedOffers[this.currentIndex].userLat, this.fetchedOffers[this.currentIndex].userLon);
-    if(!this.arraysEqual(this.previousPosition, allowActions.point)) {
+    if (!this.arraysEqual(this.previousPosition, allowActions.point)) {
       // this.currentIndex++;
       this.previousPosition = allowActions.point;
     }
@@ -323,23 +326,28 @@ export class LandingComponent implements OnInit, OnDestroy {
         this.cardArrowType = 'down';
         break;
     }
-    if (!this.canvas){
+    if (!this.canvas) {
       this.canvas = document.getElementById('kweelive');
     }
-    const cs     = getComputedStyle(this.canvas);
-    let width  = window.innerWidth;
+    const cs = getComputedStyle(this.canvas);
+    const width = window.innerWidth;
     this.changed = width;
-    let virtualWidth = parseInt( cs.getPropertyValue('width'), 10);
+    const virtualWidth = parseInt(cs.getPropertyValue('width'), 10);
     // auxiliary operations
-    let off = parseInt((this.canvas as HTMLCanvasElement).style.width, 10);
-    let p = [((1 + allowActions.point[0]/allowActions.point[3])*((virtualWidth)/2)), ((1 - allowActions.point[1]/allowActions.point[3])*(virtualWidth)/2)];
-    let x2Offset = (virtualWidth - width) / 2;
-    let xTotalOffset = virtualWidth - width;
-    let percent = (p[0] - x2Offset) / (virtualWidth - xTotalOffset);
+    const off = parseInt((this.canvas as HTMLCanvasElement).style.width, 10);
+    const p = [
+      ((1 + allowActions.point[0] / allowActions.point[3]) * ((virtualWidth) / 2)),
+      ((1 - allowActions.point[1] / allowActions.point[3]) * (virtualWidth) / 2)
+    ];
+    const x2Offset = (virtualWidth - width) / 2;
+    const xTotalOffset = virtualWidth - width;
+    const percent = (p[0] - x2Offset) / (virtualWidth - xTotalOffset);
     // x coordinate
-    if (width > 1400){
+    if (width > 1400) {
       p[0] = width * percent - ((width - 1400) / 2);
-    } else { p[0] = width * percent }
+    } else {
+      p[0] = width * percent;
+    }
     // y coordinate
     if (off === 123) {
       p[1] = (p[1]); // -144 -64
@@ -353,11 +361,13 @@ export class LandingComponent implements OnInit, OnDestroy {
   }
 
   arraysEqual(arr1, arr2) {
-    if(arr1.length !== arr2.length)
+    if (arr1.length !== arr2.length) {
       return false;
-    for(let i = arr1.length; i--;) {
-      if(arr1[i] !== arr2[i])
+    }
+    for (let i = arr1.length; i--;) {
+      if (arr1[i] !== arr2[i]) {
         return false;
+      }
     }
 
     return true;
@@ -396,10 +406,11 @@ export class LandingComponent implements OnInit, OnDestroy {
 
   // LONGITUDE -180 to + 180
   generateRandomLong() {
-    return +(Math.random() * (180 - (-180)) + (-180)).toFixed(3) * 1;
+    return +(Math.random() * (180 - (-180)) + (-180)).toFixed(3);
   }
+
   // LATITUDE -90 to +90
   generateRandomLat() {
-    return +(Math.random() * (90 - (-90)) + (-90)).toFixed(3) * 1;
+    return +(Math.random() * (90 - (-90)) + (-90)).toFixed(3);
   }
 }
